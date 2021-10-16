d689bda0f66a9badc212a497acf473d876a62bd7
Naved <navedatgames@gmail.com>
2021-10-16 19:37:45 +0530

    ticket generated

--- script.js
+++ script.js
@@ -4,7 +4,19 @@ let body = document.querySelector("body")
 addbt.addEventListener("click",function(){
     let div = document.createElement("div")
 
-    div.classList.add("modal")
+    div.innerHTML = ` <div class="modal">
+    <div class="text-area">
+        <div class="inner-text-area" contenteditable="true"></div>
+    </div>
+    <div class="priroty">
+        <div class="priroty-container">
+            <div class="inner-container pink"></div>
+            <div class="inner-container blue"></div>
+            <div class="inner-container yellow"></div>
+            <div class="inner-container black"></div>
+        </div>
+    </div>
+</div>`
 
     body.append(div)
 })
