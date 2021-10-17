f1506d4cb8312fdc1cfb6a643a68eed739ee3b8c
Naved <navedatgames@gmail.com>
2021-10-17 19:30:02 +0530

    ticket added with color change

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let addbt = document.querySelector(".add")
 let body = document.querySelector("body")
 let grid = document.querySelector(".grid")
 
+let colors = ["pink", "blue","yellow","black"]
+
 addbt.addEventListener("click",function(){  
     let preModal = document.querySelector(".modal")
     if(preModal!=null)  return;
@@ -20,6 +22,8 @@ addbt.addEventListener("click",function(){
         </div>
     </div>
 </div>`
+
+    //color choose and select
     let allModal = div.querySelectorAll(".inner-container");
     let tickerColor = "black";
     for(let i =0;i<allModal.length;i++){
@@ -32,7 +36,7 @@ addbt.addEventListener("click",function(){
            
         })
     }
-
+    //ticket creation
     let textarea = div.querySelector(".inner-text-area");
 
     textarea.addEventListener("keypress",function(e){
@@ -49,9 +53,30 @@ addbt.addEventListener("click",function(){
 
             </div>`;
 
+            //color change
+            let ticketColor = tickerDiv.querySelector(".color")
+
+            ticketColor.addEventListener("click",function(e){
+                let currentColor = e.currentTarget.classList[1];
+                console.log(currentColor)
+                let index = -1;
+                for(let i =0;i<colors.length;i++){
+                    if(currentColor==colors[i]) index = i;
+                }
+
+                index++;
+                index = index%4;
+                let newcolor = colors[index];
+
+                ticketColor.classList.remove(currentColor);
+                ticketColor.classList.add(newcolor);
+            })
+
             div.remove();
 
             grid.append(tickerDiv)
+
+           
 
            
         }
