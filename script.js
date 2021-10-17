7bcd1dafdcc48b1493dcf36578d4b3eb78c531b5
Naved <navedatgames@gmail.com>
2021-10-17 18:43:11 +0530

    new update

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let addbt = document.querySelector(".add")
 let body = document.querySelector("body")
+let grid = document.querySelector(".grid")
 
-addbt.addEventListener("click",function(){
+addbt.addEventListener("click",function(){  
+    let preModal = document.querySelector(".modal")
+    if(preModal!=null)  return;
     let div = document.createElement("div")
 
     div.innerHTML = ` <div class="modal">
@@ -13,10 +16,46 @@ addbt.addEventListener("click",function(){
             <div class="inner-container pink"></div>
             <div class="inner-container blue"></div>
             <div class="inner-container yellow"></div>
-            <div class="inner-container black"></div>
+            <div class="inner-container black selected"></div>
         </div>
     </div>
 </div>`
+    let allModal = div.querySelectorAll(".inner-container");
+    let tickerColor = "black";
+    for(let i =0;i<allModal.length;i++){
+        allModal[i].addEventListener("click",function(e){
+            for(let j =0;j<allModal.length;j++){
+                allModal[j].classList.remove("selected")
+            }
+            e.currentTarget.classList.add("selected")
+            tickerColor = e.currentTarget.classList[1];
+           
+        })
+    }
+
+    let textarea = div.querySelector(".inner-text-area");
+
+    textarea.addEventListener("keypress",function(e){
+        if(e.key=="Enter"){
+            
+    
+            let tickerDiv = document.createElement("div")
+
+            tickerDiv.innerHTML =  ` <div class="ticket">
+            <div class="color ${tickerColor}"></div>
+            <div class="id">#aed123</div>
+            <div class="task">${e.currentTarget.innerText}</div>
+
+
+            </div>`;
+
+            div.remove();
+
+            grid.append(tickerDiv)
+
+           
+        }
+    })
 
     body.append(div)
 })
